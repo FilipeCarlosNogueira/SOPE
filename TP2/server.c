e5369b2fb37d2dee7cedb4a876d4cdad6fffc118
FilipeCarlosNogueira <fcadcn@gmail.com>
2019-05-13 20:33:48 +0100

    parsingCredentials server & user. Creation of FIFO

--- TP2/server.c
+++ TP2/server.c
@@ -1,29 +1,91 @@
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "sope.h"
 
-
-int main(int argc, char const *argv[]){
+struct server
+{
         int tnum;
-        char *password;
+        char password[MAX_PASSWORD_LEN+1];   
+};
 
+struct server host;
+
+bool parsingCredentials(int argc, char const *argv[]){
         if(argc != 3) {
                 printf("Invalid Arguments Number\n");
-                return 0;
+                return false;
         }
 
-        tnum = atoi(argv[1]);
-        if(tnum > MAX_BANK_OFFICES)
+        host.tnum = atoi(argv[1]);
+        if(host.tnum > MAX_BANK_OFFICES)
                 printf("Invalid Thread Number\n");
         else
-                printf("%d ",tnum);
+                printf("%d ",host.tnum);
 
-        password = malloc(sizeof(argv[2])+1);
-        strcpy(password, argv[2]);
-        if(strlen(password) < MIN_PASSWORD_LEN || strlen(password) > MAX_PASSWORD_LEN)
+        
+        if(strlen(argv[2]) < MIN_PASSWORD_LEN || strlen(argv[2]) > MAX_PASSWORD_LEN)
                 printf("Invalid Password\n");
-        else
-                printf("%s\n", password);
+        else{
+                strcpy(host.password, argv[2]);
+                printf("%s\n", host.password);
+        }
+
+        return true;
+}
+
+//reading user request
+int readRequest(int srv_fifo_id, char *request) {
+        int n;
+        
+        do {
+                n = read (srv_fifo_id, request, 100);
+
+        }while (n>0 && *request++ != '\0');
+        
+        return (n>0); 
+}
+
+int main(int argc, char const *argv[]){
+
+        int srv_fifo_id;
+        char request[100];
+        
+        if(!parsingCredentials(argc, argv))
+                return -1;
+
+        //Destroing the server FIFO
+        if(unlink(SERVER_FIFO_PATH) == -1){
+                perror("unlink /tmp/secure_srv failed!");
+                exit(1);
+        }
+
+        //creating the server FIFO
+        if( mkfifo(SERVER_FIFO_PATH, 0666) == -1){
+                perror("/tmp/secure_srv failed!");
+                exit(1);
+        }
+
+        //openning the srv FIFO in READ_ONLY
+        if((srv_fifo_id = open(SERVER_FIFO_PATH, O_RDONLY)) == -1){
+                perror("Open server fifo failed!");
+                exit(1);
+        }
+
+        while(readRequest(srv_fifo_id,request)) printf("%s",request);
+
+
+        //Destroing the server FIFO
+        if(unlink(SERVER_FIFO_PATH) == -1){
+                perror("unlink /tmp/secure_srv failed!");
+                exit(1);
+        }
+
+        
         return 0;
 }
--- TP2/user.c
+++ TP2/user.c
@@ -1,81 +1,132 @@
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "sope.h"
 
-int main(int argc, char const *argv[]){
-        char *password;
-        int id, delay, operation, newid, saldo, iddest, montante;
-        char newpassword[MAX_PASSWORD_LEN];
+tlv_request_t client;
 
-
-        if(argc != 5 && argc != 6) {
+bool parsing(int argc, char const *argv[]){
+        if(argc != 5 && (atoi(argv[4]) == 1 || atoi(argv[4]) == 3)) {
                 printf("Invalid Arguments Number\n");
-                return 0;
+                return false;
+        }
+        if(argc != 6 && (atoi(argv[4]) == 0 || atoi(argv[4]) == 2)) {
+                printf("Invalid Arguments Number\n");
+                return false;
         }
 
-        id = atoi(argv[1]);
-        if(id > MAX_BANK_ACCOUNTS)
+        //acount_id
+        if(atoi(argv[1]) > MAX_BANK_ACCOUNTS){
                 printf("Invalid ID Number\n");
-        else
-                printf("%d ",id);
-
-        password = malloc(sizeof(argv[2])+1);
-        strcpy(password, argv[2]);
-        if(strlen(password) < MIN_PASSWORD_LEN || strlen(password) > MAX_PASSWORD_LEN)
-                printf("Invalid Password\n");
-        else
-                printf("%s ", password);
-
-        delay = atoi(argv[3]);
-        if(delay > MAX_OP_DELAY_MS)
-                printf("Invalid Delay Number\n");
-        else
-                printf("%d ", delay);
-
-        operation = atoi(argv[4]);
-        if(operation == 0 || operation == 1 || operation == 2 || operation == 3)
-                printf("%d ",operation);
-        else
-                printf("Invalid Operation Number\n");
-
-        if(operation == 0 || operation == 2) {
-                if(argc != 6) {
-                        printf("Invalid Arguments Number\n");
-                        return 0;
-                }
-
-                if(operation == 0) {
-                        newid = atoi(strtok((char *) argv[5], " "));
-                        saldo = atoi(strtok(NULL, " "));
-                        strcpy(newpassword, strtok(NULL, " "));
-
-                        if(newid > MAX_BANK_ACCOUNTS)
-                                printf("Invalid newID Number\n");
-                        else
-                                printf("%d ", newid);
-
-                        if(saldo > MAX_BALANCE || saldo < MIN_BALANCE)
-                                printf("Invalid Saldo Number\n");
-                        else
-                                printf("%d ", saldo);
-
-                        if(strlen(newpassword) < MIN_PASSWORD_LEN)
-                                printf("Invalid newPassword\n");
-                        else
-                                printf("%s\n", newpassword);
-                }
-                else {
-                        iddest = atoi(strtok((char *) argv[5], " "));
-                        montante = atoi(strtok(NULL, " "));
-
-                        if(iddest > MAX_BANK_ACCOUNTS)
-                                printf("Invalid newID Number\n");
-                        else
-                                printf("%d ", iddest);
-
-                        printf("%d ", montante);
-                }
+                return false;
         }
+        else
+                client.value.header.account_id = atoi(argv[1]);
+
+        
+        //password
+        if(strlen(argv[2]) < MIN_PASSWORD_LEN || strlen(argv[2]) < MAX_PASSWORD_LEN){
+                printf("Invalid Password\n");
+                return false;
+        }
+        else{
+                strcpy(client.value.header.password, argv[2]);
+        }
+
+        //op_delay_ms
+        if(atoi(argv[3]) > MAX_OP_DELAY_MS){
+                printf("Invalid Delay Number\n");
+                return false;
+        }
+        else{
+                client.value.header.op_delay_ms = atoi(argv[3]);
+        }
+
+        //operation
+        int operation = atoi(argv[4]);
+        int newID;
+        char password[MAX_PASSWORD_LEN + 10];
+        switch (operation)
+        {
+        case 0: //Creat acount
+
+                //Type
+                client.type = 0;
+
+                //new ID
+                newID = atoi(strtok((char *) argv[5], " "));
+                if(newID > MAX_BANK_ACCOUNTS){
+                        printf("Invalid newID Number\n");
+                        return false;
+                }
+                else
+                        client.value.create.account_id = newID;
+
+                //Balance
+                client.value.create.balance = atoi(strtok(NULL, " "));
+               
+                //Password
+                strcpy(password, strtok(NULL, " "));
+                if(strlen(password) < MIN_PASSWORD_LEN || strlen(password) > MAX_PASSWORD_LEN){
+                        printf("Invalid newPassword\n");
+                        return false;
+                }
+                else
+                        strcpy(client.value.create.password, password);
+                
+                break;
+
+        case 1: //Balance
+
+                client.type = 1; 
+                break;
+
+        case 2: //Transfer
+
+                //Type
+                client.type = 2;
+
+                //Acount ID
+                client.value.transfer.account_id = atoi(strtok((char *) argv[5], " "));
+                
+                //Amout
+                client.value.transfer.amount = atoi(strtok(NULL, " "));
+
+                break;
+
+        case 3://Shutdown
+
+                client.type = 3; 
+                break;
+        
+        default:
+                printf("Invalid Operation Number\n");
+                return false;
+                break;
+        }
+
+        //Process ID
+        client.value.header.pid = getpid();
+
+        return true;
+}
+
+
+int main(int argc, char const *argv[]){
+        
+        if(!parsing(argc, argv))
+                return -1;
+        
+        char request_msg[100];
+
+        int fd = open(SERVER_FIFO_PATH, O_WRONLY);
+
+        read(fd, request_msg, sizeof(char)*strlen(request_msg));
+       
         return 0;
 }
